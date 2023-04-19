da62cd44b3196ca5c6560bb193fa2977dc6ab3a5
adams145 <adamzerrouq21@gmail.com>
2023-04-19 16:35:33 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,23 +1,57 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <string.h> /*for atoi*/
+#include <stdlib.h> /*for exit*/
 
 /**
- * main - performs simple operations
- * @argc: argument count
+ * main - Program Entry
+ *
+ * Description: Program that takes 4 arguments where 2 are
+ *              int variables plus an operator to perform
+ *              a function between the two numbers
+ *
+ *              Usage: ./calc num1 operator num2
+ *                         |     |       |     |_ argv[3]
+ *                         |     |       |_ argv[2]
+ *                         |     |_ argv[1]
+ *                         |_ argv[0]
+ *
+ * @argc: argument counter
  * @argv: argument vector
- * Return: 0 if no errors
+ *
+ * Return: 0-on success
  */
 int main(int argc, char *argv[])
 {
-	register int a, b;
-	int (*fptr)(int, int);
+	int num1, num2;
+	char *operator;
 
+	/* Error if arguments are not 4 with the executable */
 	if (argc != 4)
-		printf("Error\n"), exit(98);
-	fptr = get_op_func(argv[2]);
-	if (!fptr)
-		printf("Error\n"), exit(99);
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	printf("%i\n", fptr(a, b));
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]); /*if argument is string convert to number*/
+	num2 = atoi(argv[3]); /* as stated above */
+	operator = argv[2];
+
+	/* Error if operator is not null or a 2d array '\*' */
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	/* Error if user tries to divide / (47) or % (37) by 0*/
+	if ((*operator == 47 || *operator == 37) && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(operator)(num1, num2));
+
 	return (0);
 }
